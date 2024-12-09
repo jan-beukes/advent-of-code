e9a859d472eae90c381158ce470d08b1312b88ab
Jan Beukes <beukes.janadam@gmail.com>
2024-12-09 23:18:29 +0200

    huh

--- 2024/stuff.zig
+++ 2024/stuff.zig
@@ -6,7 +6,6 @@ const file = "input.txt";
 const input = mem.trimRight(u8, @embedFile(file), "\n");
 
 pub fn main() !void {
-    const a = std.heap.page_allocator;
+    const alloc = std.heap.page_allocator;
     var line_iter = mem.splitSequence(u8, input, "\n");
-    var list = std.ArrayList([]const u8).init(a);
 }
