f9e9c8f45ca42bb84eda3dedf2092463cd23833b
jan-beukes <beukes.janadam@gmail.com>
2024-12-09 12:51:40 +0200

    d5

--- 2024/d5/main.zig
+++ 2024/d5/main.zig
@@ -7,6 +7,24 @@ const input = mem.trimRight(u8, @embedFile(file), "\n");
 
 const ArrayList = std.ArrayList(i32);
 
+fn compare(map: std.AutoHashMap(i32, ArrayList), lhs: i32, rhs: i32) bool {
+    if (!(map.contains(lhs) or map.contains(rhs))) return false;
+
+    const lhs_list = map.get(lhs);
+    const rhs_list = map.get(rhs);
+    if (lhs_list) |list| {
+        for (list.items) |item| {
+            if (item == rhs) return true;
+        }
+    }
+    if (rhs_list) |list| {
+        for (list.items) |item| {
+            if (item == lhs) return false;
+        }
+    }
+    return false;
+}
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const a = arena.allocator();
@@ -32,11 +50,6 @@ pub fn main() !void {
         }
     }
 
-    var it = map.keyIterator();
-    while (it.next()) |k| {
-        print("{} | {any}\n", .{ k.*, map.get(k.*).?.items });
-    }
-
     var total: i32 = 0;
     outer: while (line_iter.next()) |line| {
         var tok_it = mem.tokenizeSequence(u8, line, ",");
@@ -49,10 +62,14 @@ pub fn main() !void {
             const list = map.get(num) orelse unreachable;
             for (list.items) |item| {
                 if (mem.indexOfScalar(i32, numbers.items, item) == null) continue;
-                if (mem.indexOfScalar(i32, numbers.items, item).? < i) continue :outer;
+                // He is broken
+                if (mem.indexOfScalar(i32, numbers.items, item).? < i) {
+                    mem.sort(i32, numbers.items, map, compare);
+                    total += numbers.items[numbers.items.len / 2];
+                    continue :outer;
+                }
             }
         }
-        total += numbers.items[numbers.items.len / 2];
     }
 
     print("result: {}", .{total});
